68b01a53336ccb0d5a89d14c6e492c5623a7c9f4
Thuy-Quyen Truong <thuyquyentruong543@gmail.com>
2023-09-15 11:10:26 +0700

    📦 add route and some common components

--- src/App.js
+++ src/App.js
@@ -1,30 +1,15 @@
 import React from "react";
-import Typography from "@mui/material/Typography";
-import { Box, Link } from "@mui/material";
-
-function Copyright() {
-  return (
-    <Typography variant="body2" color="text.secondary" align="center">
-      {"Copyright © "}
-      <Link color="inherit" href="https://www.coderschool.vn">
-        CoderSchool
-      </Link>{" "}
-      {new Date().getFullYear()}
-      {"."}
-    </Typography>
-  );
-}
+import { BrowserRouter } from "react-router-dom";
+import Router from "./routes";
+import { AuthProvider } from "./contexts/AuthContext";
 
 function App() {
   return (
-    <>
-      <Box sx={{ my: 4 }}>
-        <Typography variant="h4" component="h1" gutterBottom align="center">
-          My new React app
-        </Typography>
-        <Copyright />
-      </Box>
-    </>
+    <AuthProvider>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+    </AuthProvider>
   );
 }
 
